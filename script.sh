1e05f0f0e8b6aff484db5c465ce5fb83f3f8172c
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-10-01 19:01:06 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ngrok.tgz -C /bin
+rm ngrok.tgz
+
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+dpkg -i cloudflared.deb
+rm cloudflared.deb
