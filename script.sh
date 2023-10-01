b4bd1b0284568d478fd26be1b84e8d5b5856d192
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-10-01 19:11:22 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
+apt update
+apt install netplan iptables iptables-persistent git curl wget
+
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+service sshd restart
+
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok.tgz -C /bin
 rm ngrok.tgz
